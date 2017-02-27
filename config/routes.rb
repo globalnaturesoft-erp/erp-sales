e737b00fb6298e39c686d50dfc2e766e2833a565
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-02-27 00:11:46 -0800

    fix routes for sales engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Erp::Sales::Engine.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
 		namespace :backend, module: "backend", path: "backend/sales" do
-			resources :orders do
-				collection do
-					post 'list'
-				end
-			end
+			get 'sales_orders' => 'sales#sales_orders', as: :sales_orders
+			post 'sales_orders_listing' => 'sales#sales_orders_listing', as: :sales_orders_listing
+			get 'sales_order_details' => 'sales#sales_order_details', as: :sales_order_details
 		end
 	end
 end
