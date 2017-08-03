69f6d0670a68227d557b553db32d450f283f32f9
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-08-02 21:09:12 -0700

    sales feature update and review

--- app/controllers/erp/sales/backend/sales_controller.rb
+++ app/controllers/erp/sales/backend/sales_controller.rb
@@ -8,7 +8,15 @@ module Erp
 
         # POST /orders/list
         def sales_orders_listing
-          @orders = Erp::Orders::Order.sales_orders.search(params).paginate(:page => params[:page], :per_page => 5)
+          @orders = Erp::Orders::Order.sales_orders.search(params).paginate(:page => params[:page], :per_page => 10)
+          
+          if params.to_unsafe_hash[:global_filter].present? and params.to_unsafe_hash[:global_filter][:order_from_date].present?
+            @orders = @orders.where('order_date >= ?', params.to_unsafe_hash[:global_filter][:order_from_date].to_date.beginning_of_day)
+          end
+
+          if params.to_unsafe_hash[:global_filter].present? and params.to_unsafe_hash[:global_filter][:order_to_date].present? 
+            @orders = @orders.where('order_date <= ?', params.to_unsafe_hash[:global_filter][:order_to_date].to_date.end_of_day)
+          end
           
           render layout: nil
         end
