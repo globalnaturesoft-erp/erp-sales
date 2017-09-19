9c3ec7ed8989494eb39be244f431231a4eaf097f
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-09-18 21:04:47 -0700

    remove features not in use

--- app/controllers/erp/sales/backend/sales_controller.rb
+++ app/controllers/erp/sales/backend/sales_controller.rb
@@ -10,14 +10,6 @@ module Erp
         def sales_orders_listing
           @orders = Erp::Orders::Order.sales_orders.search(params).paginate(:page => params[:page], :per_page => 10)
           
-          if params.to_unsafe_hash[:global_filter].present? and params.to_unsafe_hash[:global_filter][:order_from_date].present?
-            @orders = @orders.where('order_date >= ?', params.to_unsafe_hash[:global_filter][:order_from_date].to_date.beginning_of_day)
-          end
-
-          if params.to_unsafe_hash[:global_filter].present? and params.to_unsafe_hash[:global_filter][:order_to_date].present? 
-            @orders = @orders.where('order_date <= ?', params.to_unsafe_hash[:global_filter][:order_to_date].to_date.end_of_day)
-          end
-          
           render layout: nil
         end
         
