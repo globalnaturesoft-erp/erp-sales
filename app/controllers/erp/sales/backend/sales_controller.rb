30f86d392177758a9c0b94d83d6e75c5f41ee1ca
Hung Nguyen <tonhungstar@gmail.com>
2019-11-10 11:48:43 +0000

    thay doi chinh sach quyen cho nhan vien sales

--- app/controllers/erp/sales/backend/sales_controller.rb
+++ app/controllers/erp/sales/backend/sales_controller.rb
@@ -11,14 +11,22 @@ module Erp
         def sales_orders_listing
           authorize! :sales_sales_orders_index, nil
           
-          @orders = Erp::Orders::Order.sales_orders.search(params)
+          @orders = Erp::Orders::Order.sales_orders#.search(params)
+          
+          #if can? :sales_sales_orders_list_all, nil
+          #  @orders = @orders
+          #end
+          #
+          #if can? :sales_sales_orders_list_own, nil
+          #  @orders = @orders.where(employee_id: current_user.id)
+          #end
           
           if can? :sales_sales_orders_list_all, nil
-            @orders = @orders
+            @orders = @orders.search(params)
           end
           
           if can? :sales_sales_orders_list_own, nil
-            @orders = @orders.where(employee_id: current_user.id)
+            @orders = @orders.search(params.merge(customer_saleperson_id: current_user.id))
           end
           
           @orders = @orders.paginate(:page => params[:page], :per_page => 10)
